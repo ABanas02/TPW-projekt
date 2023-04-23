b49e41bfbc7391feaa1e9d04a3ad858003638807
Adam Banaś <242353@edu.p.lodz.pl>
2023-04-23 23:29:19 +0200

    Dodanie testu do warstwy Dane

---
+++ Projekt/DaneTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Dane;
+
+namespace DaneTest
+{
+    [TestClass]
+    public class DaneTest
+    {
+        [TestMethod]
+        public void TestCreateAPI()
+        {
+            DaneAPI daneAPI = null;
+            daneAPI = DaneAPI.CreateApi();
+            Assert.IsNotNull(daneAPI);
+            Assert.IsInstanceOfType(daneAPI, typeof(DaneAPI));
+        }
+    }
+}
